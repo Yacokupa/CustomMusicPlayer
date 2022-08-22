1663e26d9ec3ef70f02127135b66ef0dc3e1fa8f
yacokupa <yaco.ing@gmail.com>
2022-08-22 19:56:56 -0300

    Agrego el proyecto

---
+++ java.js
@@ -0,0 +1,21 @@
+//Lista de canciones
+const songList = [
+    
+    {
+        title: "Animal - Wos & Acru",
+        file: "ANIMAL.mp3",
+        cover: "1.jpg"
+    },
+
+    {
+        title: "Flaca - Calamaro",
+        File: "Flaca.mp3",
+        cover: "2.jpg"
+    },
+
+    {
+        title: "Bang Bang - Warsame",
+        File: "bangbang",
+        cover: "3.jpg"
+    },
+]
